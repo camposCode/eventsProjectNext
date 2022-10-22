b58ce3505cac5fd5b1e77ee26095a40dcb4f4440
Jose Campos <camposplaza77@gmail.com>
2022-10-22 18:49:45 +0200

    eventId, events and ...slug created

---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>The Home Page</h1>
+    </div>
+  )
+}
