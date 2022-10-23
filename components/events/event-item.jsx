398a358f22c58a6b8f807385f9f5c031de445c7c
Jose Campos <camposplaza77@gmail.com>
2022-10-23 10:45:57 +0200

    event-item and event-list created, info displayed

---
+++ components/events/event-item.jsx
@@ -0,0 +1,39 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function EventItem({ title, image, date, location, id }) {
+
+    const humanReadableDate = new Date(date).toLocaleDateString('en-US', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+    });
+
+    // const formattedAddress = location.replace(',', 'n');
+
+    const exploreLink = `/events/${ id }`;
+    
+
+    return (
+        <li>
+            <img src={ '/' + image } alt={ title } />
+            <div>
+                <div>
+                    <h2>{ title }</h2>
+                    <div>
+                        <time>{ humanReadableDate }</time>
+                    </div>
+                    <div>
+                        {/* <address>{ formatteAddress }</address> */}
+                    </div>
+                </div>
+
+                <div>
+                    <Link href={ exploreLink }>Explore Event</Link>
+                </div>
+            </div>
+        </li>
+    )
+}
+
+
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import EventList from '../components/events/event-list';
+
+import { getFeaturedEvents } from '../dummy-data'
 import styles from '../styles/Home.module.css'
 
 export default function HomePage() {
+
+  const featuredEvents = getFeaturedEvents();
+  
   return (
     <div>
-      <h1>The Home Page</h1>
+      <ul>
+        <EventList items={ featuredEvents }/>
+      </ul>
     </div>
   )
 }
