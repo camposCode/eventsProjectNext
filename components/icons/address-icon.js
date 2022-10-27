e97f0f371b73aef88562fa9a01bcec9b85e87c8c
Jose Campos <camposplaza77@gmail.com>
2022-10-27 19:09:43 +0200

    eventsummary, eventlogistic ad eventcontent with their layout created

--- components/icons/address-icon.js
+++ components/icons/address-icon.js
@@ -4,7 +4,7 @@ function AddressIcon() {
       xmlns='http://www.w3.org/2000/svg'
       fill='none'
       viewBox='0 0 24 24'
-      stroke='currentColor'
+      stroke='#ccc'
     >
       <path
         strokeLinecap='round'
